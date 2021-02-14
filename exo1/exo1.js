52b3ae07ae732b876ffd6583dcd07ccb5b28fd0a
Enzo <enzotesta@hotmail.fr>
2021-02-14 15:26:22 +0100

    exo 1

---
+++ exo1/exo1.js
@@ -0,0 +1,9 @@
+function sumOfNegatives(array) {
+    // TODO
+}
+
+
+console.log("my answer:", sumOfNegatives([1, 5, -1, -4, 3]), "correct answer: -5");
+console.log("my answer:", sumOfNegatives([-1, 1, -1, -4, 3]), "correct answer -6");
+console.log("my answer:", sumOfNegatives([1, 2]), "correct answer: 0");
+console.log("my answer:", sumOfNegatives([]), "correct answer: 0");
